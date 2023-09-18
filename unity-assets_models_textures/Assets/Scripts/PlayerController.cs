c49a5ac2cfc0ec868cdad058dff3a2e0de025955
ChongLeangUENG <leangcnrp777@gmail.com>
2023-09-18 17:01:38 +0200

    Add script and camera

---
+++ unity-assets_models_textures/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5.0f;
+    public float jumpForce = 10.0f;
+    private Rigidbody rb;
+    private bool isGrounded;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        // Check if the player is grounded
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, 0.1f);
+
+        // Handle player movement
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontalInput, 0.0f, verticalInput) * moveSpeed * Time.deltaTime;
+        transform.Translate(movement);
+
+        // Handle jumping with Spacebar
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+        Debug.Log("Is Grounded: " + isGrounded);
+    }
+}
