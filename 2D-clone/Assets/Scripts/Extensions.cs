bc58d6e6768a0e335c65115f35e9e26556bb5453
ChongLeangUENG <leangcnrp777@gmail.com>
2023-10-07 23:11:05 +0200

    Clone a Super Mario Bros. 2D game

---
+++ 2D-clone/Assets/Scripts/Extensions.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Extensions
+{
+    private static LayerMask layerMask = LayerMask.GetMask("Default");
+
+    public static bool Raycast(this Rigidbody2D rb2d, Vector2 direction)
+    {
+        if (rb2d.isKinematic)
+        {
+            return false;
+        }
+
+        float radius = 0.25f;
+        float distance = 0.375f;
+
+        RaycastHit2D hit = Physics2D.CircleCast(rb2d.position, radius, direction, distance, layerMask);
+        return hit.collider != null && hit.rigidbody != rb2d;
+    }
+}
